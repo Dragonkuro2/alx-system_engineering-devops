8af93370a9967b3e0bfe1ff5880ba4f9168e39a1
Hicham Bourezi <Hicham.bourezi@gmail.com>
2024-02-23 15:57:52 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,6 @@
-#!/usr/bin/puppet
-# install flask.
-
-package { 'pip3':
-  ensure => installed,
-}
-
-package { 'Flask':
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['pip3'],
+  provider => 'pip3'
 }
-
