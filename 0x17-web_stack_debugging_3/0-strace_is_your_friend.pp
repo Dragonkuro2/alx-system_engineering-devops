cdbbfb2ffbc0ac59016e72cc084a9ca5bd695653
Dragonkuro2 <hicham.bourezi@gmail.com>
2024-04-15 14:33:16 -0700

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Creating a manifest that fix all termintion of phpp
+
+
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
